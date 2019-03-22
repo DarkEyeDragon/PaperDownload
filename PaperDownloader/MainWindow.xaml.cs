32425d94bb1c72561933a2bff51b30d81f05c202
DarkEyeDragon <joshua.tack@student.vives.be>
2019-03-22 17:07:40 +0100

    Add project files.

---
+++ PaperDownloader/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using PaperDownloader.Servers.Versions;
+
+namespace PaperDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Project ProjectType { get; set; }
+        public string Version { get; set; }
+        public string Build { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBoxProject.Items.Add(Project.Paper);
+            ComboBoxProject.Items.Add(Project.Waterfall);
+            ComboBoxProject.Items.Add(Project.Travertine);
+            ComboBoxProject.SelectedItem = ComboBoxProject.Items[0];
+            Build = "latest";
+        }
+
+        private void ComboBoxProject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxVersions.Items.Clear();
+
+            PopulateVersion();
+
+
+            if (CheckBoxManual.IsVisible && CheckBoxManual.IsChecked == true)
+            {
+                PopulateBuilds();
+            }
+        }
+        private void ComboBoxVersions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CheckBoxManual.IsChecked == true)
+            {
+                PopulateBuilds();
+            }
+        }
+
+        private void CheckBoxIsLatest_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (CheckBoxManual.IsChecked == true)
+            {
+                CheckBoxManual.Visibility = Visibility.Hidden;
+                PopulateBuilds();
+            }
+        }
+        private void ButtonDownloadJar_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (CheckBoxManual.IsChecked == false)
+            {
+                Build = "latest";
+            }
+
+            Projects.DownloadChangeEvent += UpdateDownload;
+            Projects.DownloadCompleted += DownloadCompleted;
+            Projects.DownloadJar(ProjectType, Version, Build);
+        }
+
+
+        private void DownloadCompleted()
+        {
+            Projects.DownloadChangeEvent -= UpdateDownload;
+            Projects.DownloadCompleted -= DownloadCompleted;
+            var result = MessageBox.Show("Do you want to start the server now?", "Start server",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+            ProgressBarDownload.Value = 0;
+
+        }
+
+        private void UpdateDownload(int progress)
+        {
+            ProgressBarDownload.Value = progress;
+        }
+
+        private void PopulateVersion()
+        {
+            ProjectType = (Project)ComboBoxProject.SelectedItem;
+            foreach (var stringVersion in Projects.GetVersions(ProjectType))
+            {
+                ComboBoxVersions.Items.Add(stringVersion);
+            }
+
+            Version = ComboBoxVersions.Items[0].ToString();
+            ComboBoxVersions.SelectedItem = ComboBoxVersions.Items[0];
+        }
+
+        private void PopulateBuilds()
+        {
+            ComboBoxBuilds.Items.Clear();
+            Version = ComboBoxVersions.Text;
+            string[] builds = Projects.GetBuild(ProjectType, Version);
+            if (builds == null)
+            {
+                ComboBoxBuilds.Items.Add("No builds found");
+                return;
+            }
+
+            ComboBoxBuilds.Items.Add("latest");
+            foreach (var build in builds)
+            {
+                ComboBoxBuilds.Items.Add(build);
+            }
+
+            Build = ComboBoxBuilds.Items[1].ToString();
+            ComboBoxBuilds.SelectedItem = ComboBoxBuilds.Items[1];
+        }
+
+        private void ButtonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                IsFolderPicker = true
+            };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Projects.DownloadPath = dialog.FileName;
+                TextBoxPath.Text = dialog.FileName;
+            }
+        }
+
+        private void ComboBoxBuilds_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxBuilds.SelectedItem is string && ComboBoxBuilds.SelectedItem.Equals("latest"))
+            {
+                CheckBoxManual.Visibility = Visibility.Visible;
+                CheckBoxManual.IsChecked = false;
+                Build = ComboBoxBuilds.Items[1].ToString();
+                ComboBoxBuilds.SelectedItem = ComboBoxBuilds.Items[1];
+            }
+        }
+    }
+}
---
+++ PaperDownloader/Servers/Versions/Projects.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PaperDownloader.Servers.Versions
+{
+    public enum Project
+    {
+        Paper,
+        Waterfall,
+        Travertine
+    };
+
+    class Projects
+    {
+        private static string _baseUrl = "https://papermc.io/api/v1/";
+
+        public static string DownloadPath { get; set; }
+
+
+        private static readonly string USER_AGENT = $"{Assembly.GetEntryAssembly().GetName().Name}/{Assembly.GetEntryAssembly().GetName().Version} ({AssemblyContentType.WindowsRuntime})";
+
+        public static string[] GetVersions(Project project)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add(HttpRequestHeader.UserAgent, USER_AGENT);
+                var json = wc.DownloadString($"{_baseUrl}{project.ToString().ToLower()}");
+                Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                foreach (var keyValuePair in result)
+                {
+                    if (keyValuePair.Key.Contains("version"))
+                    {
+                        JArray array = (JArray) keyValuePair.Value;
+                        return array.Select(jv => (string) jv).ToArray();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static string[] GetBuild(Project project, string version)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add(HttpRequestHeader.UserAgent, USER_AGENT);
+                var json = wc.DownloadString($"{_baseUrl}{project.ToString().ToLower()}/{version}");
+                Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                foreach (var keyValuePair in result)
+                {
+                    if (keyValuePair.Key.Contains("builds"))
+                    {
+                        var buildNum =
+                            JsonConvert.DeserializeObject<Dictionary<string, object>>(keyValuePair.Value.ToString());
+                        foreach (var valuePair in buildNum)
+                        {
+                            if (valuePair.Key.Equals("all"))
+                            {
+                                JArray array = (JArray) valuePair.Value;
+                                return array.Select(jv => (string) jv).ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Download the external Paper jar trough their API
+        /// </summary>
+        /// <param name="project">Projects of the the type <see cref="Project"/></param>
+        /// <param name="version">Minecrafts target version</param>
+        /// <param name="build">The build number of the jar</param>
+        public static void DownloadJar(Project project, string version, string build)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string localPath = $"{DownloadPath}/{project}-{version}_{build}.jar";
+                wc.Headers.Add(HttpRequestHeader.UserAgent, USER_AGENT);
+                wc.DownloadFileAsync(new Uri(
+                        $"{_baseUrl}{project.ToString().ToLower()}/{version}/{build}/download"),
+                    localPath);
+
+                wc.DownloadProgressChanged += DownloadProgressChanged;
+                wc.DownloadFileCompleted += DownloadFileCompleted;
+            }
+        }
+
+
+        public delegate void DownloadChangeEventHandler(int progress);
+
+        public static event DownloadChangeEventHandler DownloadChangeEvent;
+
+        public delegate void DownloadCompletedHandler();
+
+        public static event DownloadCompletedHandler DownloadCompleted;
+
+        private static void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DownloadChangeEvent?.Invoke(e.ProgressPercentage);
+            Debug.WriteLine($"{e.ProgressPercentage}%");
+        }
+
+        private static void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            DownloadCompleted?.Invoke();
+        }
+    }
+}
